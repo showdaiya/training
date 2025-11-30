331d719fbc55fd43318694c756af4c2291883278
akai <104837592+showdaiya@users.noreply.github.com>
2025-11-30 20:38:29 +0900

    Delete src/main/java/com/example/large/membersystem/shared directory

--- src/main/java/com/example/large/membersystem/shared/SharedLogicStatus.java
+++
@@ -1,8 +0,0 @@
-// File: src/main/java/com/example/large/membersystem/shared/SharedLogicStatus.java
-package com.example.large.membersystem.shared;
-
-public enum SharedLogicStatus {
-    SUCCESS,
-    BUSINESS_ERROR,
-    SYSTEM_ERROR
-}
